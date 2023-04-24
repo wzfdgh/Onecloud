4776915cdc00b14fb44e8d09cead91c99d99bbde
王子风度 <93830081+wzfdgh@users.noreply.github.com>
2023-04-24 12:44:27 +0800

    Update arm.sh

--- arm.sh
+++ arm.sh
@@ -14,8 +14,10 @@ read -e -p " 请输入 [0-2]:" num
 case "$num" in
 0)
     mkdir ${D} && cp -r /.config ${D} && cp /etc/apt/sources.list ${D} && cp /etc/vsftpd.conf ${D} && cp /etc/rc.local ${D} && cp /etc/network/interfaces ${D} && cp * /etc/netplan ${D}
+    ;;
 1)
     echo none > /sys/class/leds/onecloud:blue:alive/trigger && echo none > /sys/class/leds/onecloud:red:alive/trigger && echo none > /sys/class/leds/onecloud:green:alive/trigger && echo 0 > /sys/class/leds/onecloud:blue:alive/brightness && echo 0 > /sys/class/leds/onecloud:red:alive/brightness && echo 0 > /sys/class/leds/onecloud:green:alive/brightness && cp ${D}/sources.list /etc/apt && echo 'deb http://download.opensuse.org/repositories/home:/nikoneko:/test/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/home:nikoneko:test.list && curl -fsSL https://download.opensuse.org/repositories/home:nikoneko:test/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_nikoneko_test.gpg > /dev/null && apt update && apt install qbittorrent-enhanced-nox vsftpd && cp -r ${D}/.config / && cp ${D}/vsftpd.conf /etc && rm /etc/ftpusers && cp ${D}/armbian-default.yaml /etc/netplan && cp ${D}/interfaces /etc/network && netplan apply && netplan generate
+    ;;
 2)
     wget -qO- "api.github.com/repos/hzyitc/armbian-onecloud/releases/latest" | grep -o '"tag_name": ".*"' | cut -d'"' -f4
     ;;
